495e402607757d3805fd56be2b87874780c0fdb5
taku-hira <goldenslumbers0902@gmail.com>
2025-11-09 22:36:51 +0900

    Initial Commit

---
+++ ChatViewWithSwiftUI/ChatViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUIApp.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by 平河 on 2025/11/09.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

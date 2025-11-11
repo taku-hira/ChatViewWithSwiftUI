8c7de83180c28297fa91b6df0453723e6f5d8214
taku-hira <goldenslumbers0902@gmail.com>
2025-11-11 23:03:10 +0900

    inputArea

--- Views/ChatView.swift
+++ Views/ChatView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ChatView: View {
+    
+    @State private var textFieldText: String = ""
+    
     var body: some View {
         VStack(spacing: 0) {
             // MessageArea
@@ -41,16 +44,26 @@ extension ChatView {
     
     private var inputArea: some View {
         HStack {
-            Circle()
-                .frame(width:40, height: 40)
-            Circle()
-                .frame(width:40, height: 40)
-            Circle()
-                .frame(width:40, height: 40)
-            Capsule()
-                .frame(height:40)
-            Circle()
-                .frame(width:40, height: 40)
+            HStack {
+                Image(systemName: "plus")
+                Image(systemName: "camera")
+                Image(systemName: "photo")
+            }
+            .font(.title2)
+            TextField("Aa", text: $textFieldText)
+                .padding()
+                .background(Color(uiColor: .secondarySystemBackground))
+                .clipShape(Capsule())
+                .overlay(
+                    Image(systemName: "face.smiling")
+                        .font(.title)
+                        .padding()
+                        .foregroundColor(.gray)
+                    
+                    , alignment: .trailing
+                )
+            Image(systemName: "mic")
+                .font(.title2)
         }
         .padding()
         .background(.white)
